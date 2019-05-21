ad92bbfe8b99e760d639d5b5315d597de90f6810
Jan Mirecki <mirecki.janek@gmail.com>
2019-05-21 16:10:30 +0200

    Updated package.json & eslintrc
    - added husky + lint-staged
    - updated eslint config for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,26 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'airbnb-typescript',
     'plugin:react/recommended',
+    'plugin:jest/recommended',
     'prettier',
-    'prettier/react'
+    'prettier/react',
   ],
   globals: {
-    'window': true,
-    'document': true
-  }
+    window: true,
+    document: true,
+  },
+  env: {
+    'jest/globals': true,
+  },
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
 }
