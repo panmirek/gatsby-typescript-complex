79dc408a25ef46794a8461346b05e843c574d6cb
Jan Mirecki <mirecki.janek@gmail.com>
2019-05-21 21:39:15 +0200

    Updated site metadata in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kłosy – Professional chefs knives`,
+    description: `Here you can customize and order your own Kłosy knives set.`,
+    author: `@panmirek`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Kłosy – knife maker`,
+        short_name: `Kłosy`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -28,8 +28,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
