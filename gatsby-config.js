0b4c85d168b9c5685405c21d861d6839869c00dd
Jan Mirecki <mirecki.janek@gmail.com>
2019-05-22 02:59:44 +0200

    Updated package meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Kłosy – Professional chefs knives`,
-    description: `Here you can customize and order your own Kłosy knives set.`,
+    title: `GatsbyJS Typescript complex starter`,
+    description: `This is a complex GatsbyJS configuration. It's prepared for TypeScript and it's fully integrated with: eslint, stylelint, prettier, styled-components, root imports, commit hooks (husky) and jest. Tested with WebStorm v.2019.1.2 & VS Code v.1.34.0`,
     author: `@panmirek`,
   },
   plugins: [
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Kłosy – knife maker`,
-        short_name: `Kłosy`,
+        name: `GatsbyJS Typescript complex starter`,
+        short_name: `gatsby-typescript-complex`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
